32950f507273956d4579b3676986a5783d096493
iangifford <lightyagamie10@gmail.com>
2019-03-28 19:29:00 -0700

    building and solving the remove method

---
+++ PhilsLendingLibrary/PhilsLendingLibrary/Classes/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhilsLendingLibrary.Classes
+{
+    public class Library<T> : IEnumerable
+    {
+        T[] books = new T[1];
+        int counter = 0;
+
+        public void AddBook(T book)
+        {
+            if(counter == books.Length)
+            {
+                Array.Resize(ref books, (books.Length * 2));
+            }
+            books[counter++] = book;
+        }
+
+        public void RemoveBook(T book)
+        {
+            T[] tempArr = null;
+
+        }
+
+
+
+
+
+
+
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < counter; i++)
+            {
+                yield return books[i];
+                
+            }
+        }
+
+        //MAGIC like amanda says
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+  
+
+}
---
+++ PhilsLendingLibrary/PhilsLendingLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PhilsLendingLibrary.Classes;
+
+
+namespace PhilsLendingLibrary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool Borrow()
+        {
+
+            return 0;
+        }
+
+    }
+}
